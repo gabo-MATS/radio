677693c4b7ea57aea5627cfd403b48d0d3391d2f
gabo-MATS <gabrielmartinez1222@gmail.com>
2016-07-29 09:01:05 -0600

    radio

---
+++ radio.py
@@ -0,0 +1,45 @@
+class Radio():
+	def __init__(self,marca,):
+		self.marca=marca
+		self.encendido=False
+		self.volumen= 0
+		self.emisora_AM=300
+		self.emisora_FM=87.0
+		self.en_FM=True
+	def encender(self):
+		self.encendido=True
+	def apagar(self):
+		self.encendido=False
+	def subir_volumen(self):
+		if volumen >=100:
+			self.volumen =100
+		else :
+			self.volumen+=5
+	def bajar_volumen(self):
+		if volumen <=0:
+			self.volumen =0
+		else :
+			self.volumen-=5
+	def subir_emisora(self):
+		if not self.en_FM:
+			if self.emisora_AM >=1300:
+				self.emisora_AM =300
+			else :
+				self.emisora_AM+=40	
+		else :
+			if self.emisora_FM >=107.0:
+				self.emisora_FM =87
+			else :
+				self.emisora_FM+=0.5
+
+	def bajar_emisora(self):
+		if not self.en_FM:
+			if self.emisora_AM <=3300:
+				self.emisora_AM =1300
+			else :
+				self.emisora_AM-=40	
+		else :
+			if self.emisora_FM <=87.0:
+				self.emisora_FM =107
+			else :
+				self.emisora_FM-=0.5
